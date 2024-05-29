4d219d1293ad1a276e2d7b91be814e5301f3ec65
gabriel <gabrielcoloradogvda@gmail.com>
2024-05-29 03:19:35 -0300

    config app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "express-async-errors";
 import { handleErrors } from "./errors";
+import api from "../api"
 import express, { Application } from "express";
 import userRoutes from "./routers/users.routes";
 import loginRoutes from "./routers/login.routes";
@@ -12,6 +13,8 @@ app.use(express.json())
 app.use("/users", userRoutes)
 app.use("/product", productRoutes)
 app.use("/login", loginRoutes)
+
+app.use("/api/v1", api)
 
 app.use(handleErrors)
 export default app
