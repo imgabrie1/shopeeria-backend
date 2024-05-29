16c7e7722ed66e19c601596d15f05db327f6add2
gabriel <gabrielcoloradogvda@gmail.com>
2024-05-29 03:57:13 -0300

    v1 shopeeria

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import "express-async-errors";
 import { handleErrors } from "./errors";
-import api from "../api"
 import express, { Application } from "express";
 import userRoutes from "./routers/users.routes";
 import loginRoutes from "./routers/login.routes";
@@ -13,8 +12,6 @@ app.use(express.json())
 app.use("/users", userRoutes)
 app.use("/product", productRoutes)
 app.use("/login", loginRoutes)
-
-app.use("/api/v1", api)
 
 app.use(handleErrors)
 export default app
