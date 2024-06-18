ac02af9196d7f987062f02410aadb5b973c7e5e3
gabriel <gabrielcoloradogvda@gmail.com>
2024-06-18 01:16:18 -0300

    feat: remove cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import "express-async-errors";
 import { handleErrors } from "./errors";
 import express, { Application } from "express";
-import cors from "cors";
 import userRoutes from "./routers/users.routes";
 import loginRoutes from "./routers/login.routes";
 import productRoutes from "./routers/products.routes";
@@ -10,9 +9,6 @@ const app: Application = express()
 app.use(express.json())
 
 
-app.use(cors({
-    origin: "http://localhost:5173"
-  }));
 app.use("/users", userRoutes)
 app.use("/product", productRoutes)
 app.use("/login", loginRoutes)
